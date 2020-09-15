65d93c6a7d967f15d285277168a8e2c43043964a
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-16 01:17:50 +0900

    inputing all of the information from the API call

--- src/App.js
+++ src/App.js
@@ -9,22 +9,56 @@ import key from './key';
 class App extends Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+      city: undefined,
+      country: undefined,
+      weather: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
     this.getWeather();
   }
 
   getWeather = async () => {
-    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=${key}`);
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=seattle&appid=${key}`);
   
     const response = await apiCall.json();
 
-    console.log(response.coord.lon);
+    console.log(response);
+
+    this.setState({ 
+      city: response.name,
+      country: response.sys.country,
+      weather: response.weather,
+      icon: response.weather[0].icon,
+      // main: ,
+      celsius: response.main.temp,
+      temp_max: response.main.temp_max,
+      temp_min: response.main.temp_min,
+      description: response.weather,
+      error: false
+    })
   }
   
   render() {
     return(
       <div className="App">
-        <Weather />
+        <Weather 
+          city={this.state.city} 
+          country={this.state.country}
+          celsius={this.state.celsius} 
+          weather={this.state.weather}
+          icon={this.state.icon} 
+          main={this.state.main}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
       </div>
     );
   }
--- src/components/weather_component.jsx
+++ src/components/weather_component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Weather = () => {
+const Weather = (props) => {
   return(
     <div className="container">
       <div className="cards">
-        <h1>London</h1>
+        <h1>{props.city},{props.country}</h1>
         <h5 className="py-4">
           <i className="wi wi-day-sunny display-1"></i>
         </h5>
-        <h1 className="py-2">25&deg;</h1>
+        <h1 className="py-2">{Math.floor(props.celsius - 273.15)}&deg;</h1>
         {/** show maxx and min */}
-        {minmaxTemp(24, 19)}
+        {minmaxTemp(Math.floor(props.temp_min - 273.15), Math.floor(props.temp_max - 273.15))}
         <h4 className="py-3">
-          Slow rain
+          {/* {props.description[0]} */}
         </h4>
       </div>
     </div>
