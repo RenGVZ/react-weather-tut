bc5ced7e6627502a36bd448627e05592c5e04a03
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-16 00:36:04 +0900

    figured out how to use api call to get right info and hid key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css'
+import Weather from './components/weather_component';
+import key from './key';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=${key}`);
+  
+    const response = await apiCall.json();
+
+    console.log(response.coord.lon);
+  }
+  
+  render() {
+    return(
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
